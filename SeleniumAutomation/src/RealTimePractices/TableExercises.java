349b6b43f85cecfbcd9f16b7e029aefcaf6ac730
Ashfat Al Rashid <61910244+AshfatAbegh@users.noreply.github.com>
2024-09-09 16:46:13 +0600

    Update TableExercises.java

--- SeleniumAutomation/src/RealTimePractices/TableExercises.java
+++ SeleniumAutomation/src/RealTimePractices/TableExercises.java
@@ -14,14 +14,13 @@ public class TableExercises {
 		
 		driver.get("https://www.cricbuzz.com/live-cricket-scorecard/66446/gt-vs-srh-62nd-match-indian-premier-league-2023");
 	    
-		WebElement Table = driver.findElement(By.cssSelector("div[class='cb-col cb-col-100 cb-ltst-wgt-hdr']"));
-	    int RowCount = Table.findElements(By.cssSelector("cb-col cb-col-100 cb-scrd-itms")).size();
-	    int count = Table.findElements(By.cssSelector("div[class='cb-col cb-col-100 cb-scrd-itms'] div:nth-child(3)")).size();
+	        WebElement Table = driver.findElement(By.cssSelector("div[class='cb-col cb-col-100 cb-ltst-wgt-hdr']"));
+	          int RowCount = Table.findElements(By.cssSelector("cb-col cb-col-100 cb-scrd-itms")).size();
+	          int count = Table.findElements(By.cssSelector("div[class='cb-col cb-col-100 cb-scrd-itms'] div:nth-child(3)")).size();
 	    
-	    for(int i =0; i<count; i++)
-	    {
-	    	System.out.println(Table.findElements(By.cssSelector("div[class='cb-col cb-col-100 cb-scrd-itms'] div:nth-chilld(3)")).get(i).getText());
-	    }
-	    
+	        for(int i =0; i<count; i++)
+	        {
+	    	  System.out.println(Table.findElements(By.cssSelector("div[class='cb-col cb-col-100 cb-scrd-itms'] div:nth-chilld(3)")).get(i).getText());
+	        }
 	  }
 }
